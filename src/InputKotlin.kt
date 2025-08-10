5225063292df21d9055e505ad718fc46c0e8ae9c
sukhpreetsing04 <sukhpreetsingh19289@gmail.com>
2025-08-10 22:10:08 +0530

    Getting Started with Kotline day1

---
+++ src/InputKotlin.kt
@@ -0,0 +1,14 @@
+//fun main() {
+//    print("Enter text: ")
+//    val input = readln()
+//    print("You entered: $input")
+//}
+
+import java.util.Scanner
+
+fun main() {
+    val number1 = Scanner(System.`in`)
+    print("Enter an integer: ")
+    var enterednumber1:Int = number1.nextInt()
+    println("You entered: $enterednumber1")
+}
---
+++ src/Operator.kt
@@ -0,0 +1,28 @@
+fun main() {
+    val a = 10 + 20
+    val b = 23 * 13 - 3 + 6 / 5
+    /*
+    * 23 * 13 - 3 + 1
+    * 299 - 3 + 1
+    * 299 - 2
+    * 297
+    * */
+    println(a)
+    print(b)
+
+    /*
+    * operators in kotlin
+    * arithmetic : Addition +, subtraction -, multiplication *, division /, modulus %
+    * relational : > (greater than), < (less than), >= (greater than or equals to), <= (smaller than or equals to), == equal to, !- not equal to
+    * assignment: =, +=, -=, /= , *=, %=
+    * unary: ++, -- (a++ or ++a) (--a or a--)
+    * logical: &&, ||, !
+    * bitwise: shl (signed shift left)
+    *          shr (signed shift right)
+    *          ushr (unsigned shift right)
+    *          and (bitwise and)
+    *          or  (bitwise or)
+    *          xor (bitwise xor)
+    *          inv (bitwise inverse)
+    */
+}
